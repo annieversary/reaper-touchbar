8eb89f0967091f0d4c798383839b18d5bcff6b7b
annieversary <annie@versary.town>
2023-01-07 01:39:16 +0100

    display volume value

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 use reaper_high::Reaper;
 use reaper_macros::reaper_extension_plugin;
-use reaper_medium::{CommandId, HookCommand};
+use reaper_medium::{CommandId, ControlSurface, HookCommand};
 use rubrail::ItemId;
 use std::error::Error;
 
 use rubrail::TTouchbar;
 use rubrail::Touchbar;
 
-static mut TB: Option<&'static mut rubrail::touchbar::RustTouchbarDelegateWrapper> = None;
-static mut LABEL_ID: Option<ItemId> = None;
+static mut STATE: Option<State> = None;
 
 #[reaper_extension_plugin(name = "reaper-touchbar", support_email_address = "annie@versary.town")]
 fn plugin_main() -> Result<(), Box<dyn Error>> {
@@ -16,6 +15,9 @@ fn plugin_main() -> Result<(), Box<dyn Error>> {
 
     session
         .plugin_register_add_hook_command::<MyHookCommand>()
+        .unwrap();
+    session
+        .plugin_register_add_csurf_inst(Box::new(MyControlSurface))
         .unwrap();
 
     // let reaper = session.reaper();
@@ -39,11 +41,27 @@ fn plugin_main() -> Result<(), Box<dyn Error>> {
 
     // leak cause we need it to be up for the whole program
     unsafe {
-        TB = Some(Box::leak(tb));
-        LABEL_ID = Some(label1_id);
+        STATE = Some(State {
+            tb: Box::leak(tb),
+            label_id: label1_id,
+        });
     }
 
     Ok(())
+}
+
+struct State {
+    tb: &'static mut rubrail::touchbar::RustTouchbarDelegateWrapper,
+    label_id: ItemId,
+}
+
+fn update_label(s: &str) {
+    unsafe {
+        let Some(state) = &mut STATE else {
+            return;
+        };
+        state.tb.update_label(&state.label_id, s);
+    }
 }
 
 struct MyHookCommand;
@@ -51,22 +69,28 @@ struct MyHookCommand;
 impl HookCommand for MyHookCommand {
     fn call(command_id: CommandId, _flag: i32) -> bool {
         // let r = Reaper::get().medium_reaper();
-
-        // TODO check if selecting a track is an action
-
         // r.show_console_msg(format!("Executing command {command_id}!\n"));
 
+        update_label(&format!("Last command: {command_id}"));
+
+        false
+    }
+}
+
+#[derive(Debug)]
+struct MyControlSurface;
+
+impl ControlSurface for MyControlSurface {
+    fn on_track_selection(&self, args: reaper_medium::OnTrackSelectionArgs) {
+        let r = Reaper::get().medium_reaper();
         unsafe {
-            'tb: {
-                let Some(tb) = &mut TB else {
-                    break 'tb;
-                };
-                let Some(id) = LABEL_ID else {
-                    break 'tb;
-                };
-                tb.update_label(&id, &format!("Last command: {command_id}"));
+            if let Ok(v) = r.get_track_ui_vol_pan(args.track) {
+                update_label(&format!("Track volume: {}", v.volume.get()));
             }
         }
-        false
+    }
+
+    fn set_surface_volume(&self, args: reaper_medium::SetSurfaceVolumeArgs) {
+        update_label(&format!("Track volume: {}", args.volume.get()));
     }
 }
